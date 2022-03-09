3d40e9e8a6f7ad0ba7891068b30281725a28c14a
Danilo <danilo.sants@live.com>
2022-03-09 07:59:20 +0000

    using stack and adding background image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:myapp/home_page.dart';
 
@@ -12,60 +16,87 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 300,
+                    child: Image.asset('assets/images/Logo.png'),
+                  ),
+                  Container(height: 20),
+                  Card(
+                      child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        // ignore: prefer_const_constructors
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ]),
+                  )),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (email == 'danilo@test.com' &&
+                          password == 'test#123') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('falha na autenticação');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300,
-                height: 300,
-                child: Image.network(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png'),
-              ),
-              Container(height: 20),
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: "Email", border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                onChanged: (text) {
-                  password = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: "Password", border: OutlineInputBorder()),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              RaisedButton(
-                  onPressed: () {
-                    if (email == 'danilo@test.com' && password == 'test#123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('falha na autenticação');
-                    }
-                  },
-                  child: Text('Entrar'))
-            ],
-          ),
-        ),
-      ),
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpeg',
+              fit: BoxFit.cover,
+            )),
+        Container(color: Colors.black.withOpacity(0.1)),
+        _body(),
+      ],
     ));
   }
 }
